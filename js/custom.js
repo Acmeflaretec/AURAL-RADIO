d616c86e27d25da1247a5ee9d51299bf7aab2cc2
abdusamad8575 <samadpts786313@gmail.com>
2024-04-06 18:26:36 +0530

    updation completed

--- js/custom.js
+++ js/custom.js
@@ -107,3 +107,25 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+	scrollFunction();
+ };
+ 
+ function scrollFunction() {
+	var backToTopBtn = document.getElementById("backToTopBtn");
+ 
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	   backToTopBtn.style.display = "block";
+	} else {
+	   backToTopBtn.style.display = "none";
+	}
+ }
+ 
+ // Scroll to the top of the document when the button is clicked
+ function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+ }
+ 
